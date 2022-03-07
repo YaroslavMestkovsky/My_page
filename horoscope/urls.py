21a83ca4566a57c2a49d3162313dce7dcd1d7dc9
79608 <mestkovskyyaroslavd@mail.ru>
2022-03-08 01:24:41 +0300

    Добавлены три конвертера:     1) конвертер 4х цифр     2) конвертер вещественных цифр     3) конвертер дат

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
-from . import views as views
+from django.urls import path, register_converter
+from . import views as views, converters
+
+register_converter(converters.FourDigitConverter, 'yyyy')
+register_converter(converters.FloatDigitConverter, 'float')
+register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
     path('', views.index),
+    path('<date:number>', views.get_date_converters),
     path('type', views.show_types),
     path('type/<str:element>', views.show_element_signs, name='type_name'),
     path('<int:month>/<int:day>', views.day_month),
+    path('<yyyy:number>', views.get_yyyy_converters),
     path('<int:sign_zodiac>', views.get_info_about_zodiac_sign_by_number),
+    path('<float:number>', views.get_float_converters),
     path('<str:sign_zodiac>', views.get_info_about_zodiac_sign, name='horoscope_name'),
 ]
