ac4c84f86630c96282465652ae581813a2c8e06c
79608 <mestkovskyyaroslavd@mail.ru>
2022-03-08 00:40:21 +0300

    Добавлены предположения по знаку исходя из указанных дня и месяца.

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -3,8 +3,9 @@ from . import views as views
 
 urlpatterns = [
     path('', views.index),
-    path('type/', views.show_types),
+    path('type', views.show_types),
     path('type/<str:element>', views.show_element_signs, name='type_name'),
+    path('<int:month>/<int:day>', views.day_month),
     path('<int:sign_zodiac>', views.get_info_about_zodiac_sign_by_number),
     path('<str:sign_zodiac>', views.get_info_about_zodiac_sign, name='horoscope_name'),
 ]
