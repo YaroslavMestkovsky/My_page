22936043b95ff9d5b194c8e0cee6f052d1f42f37
79608 <mestkovskyyaroslavd@mail.ru>
2022-03-09 21:24:53 +0300

    Улучшены шаблоны, добавлен базовый HTML и оформлено наследование шаблонов

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -6,7 +6,7 @@ register_converter(converters.FloatDigitConverter, 'float')
 register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='horoscope_index'),
     path('kiany', views.get_kiany_info),
     path('guinnessworldrecords', views.get_guinness_world_records),
     path('<date:number>', views.get_date_converters),
